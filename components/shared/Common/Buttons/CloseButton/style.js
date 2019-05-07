cd98bea3189a6f2af304a2752200e16e3635cdef
Garou <jrgarou@gmail.com>
2019-05-07 15:44:15 -0300

    Closebutton with zIndex Modal #882 #discussion_r281705004

--- components/shared/Common/Buttons/CloseButton/style.js
+++ components/shared/Common/Buttons/CloseButton/style.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 import theme from '@emcasa/ui'
 import Button from 'components/shared/Common/Buttons'
+import {zIndexModal} from 'constants/zIndex'
 
 export default styled(Button)`
-  z-index: 5;
+  z-index: ${zIndexModal};
   position: absolute;
   top: ${theme.space[2]}px;
   right: ${theme.space[2]}px;
