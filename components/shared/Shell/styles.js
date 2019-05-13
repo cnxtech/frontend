6e78f5670277220542f64b4102c37bd1133c1062
Lucas Tulio <lucasctulio@gmail.com>
2019-05-13 19:38:26 -0300

    Fix header dimension import and usage

--- components/shared/Shell/styles.js
+++ components/shared/Shell/styles.js
@@ -1,12 +1,12 @@
 import styled, { css } from 'styled-components'
 import {HEADER_HEIGHT} from 'constants/dimensions'
-import {MAX_HEADER_HEIGHT} from 'components/shared/Shell/Header/styles'
+import {HEADER_SEARCH_HEIGHT} from 'constants/dimensions'
 
 export default styled.div`
   display: flex;
   flex-direction: column;
-  min-height: ${props => props.search ? `calc(100vh - ${MAX_HEADER_HEIGHT}px)` : `calc(100vh - ${HEADER_HEIGHT}px)`};
-  margin-top: ${(props) => props.transparentHeader ? null : `${props.search ? MAX_HEADER_HEIGHT : HEADER_HEIGHT}px`};
+  min-height: ${props => props.search ? `calc(100vh - ${HEADER_SEARCH_HEIGHT}px)` : `calc(100vh - ${HEADER_HEIGHT}px)`};
+  margin-top: ${(props) => props.transparentHeader ? null : `${props.search ? HEADER_SEARCH_HEIGHT : HEADER_HEIGHT}px`};
   box-sizing: border-box;
 `
 
