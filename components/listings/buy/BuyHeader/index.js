1970b78f61665707cda45e7ad31c174d4d4bc319
Garou <jrgarou@gmail.com>
2019-05-14 19:26:54 -0300

    Responsive font-size

--- components/listings/buy/BuyHeader/index.js
+++ components/listings/buy/BuyHeader/index.js
@@ -10,12 +10,12 @@ class BuyHeader extends Component {
     return (
       <Wrapper>
         <Container>
-          <Title fontWeight="bold">
+          <Title fontSize={[3, null, null, 4]}>
             Busque seu imóvel <TextHighlight>sem sair de casa</TextHighlight> e{' '}
             <TextHighlight>sem corretores</TextHighlight>{' '}
             <TextHighlight strokeWidth="3">empurrando imóveis</TextHighlight>.
           </Title>
-          <Text textAlign="center" fontSize="small">
+          <Text fontSize={[1, null, null, 2]}>
             Visite imóveis através do <strong>Tour Virtual</strong> e fale com
             um dos nossos <strong>Especialistas</strong> de onde estiver.
           </Text>
