0bd1773ab2089b5bddc625be6087aa29eb647e7c
Garou <jrgarou@gmail.com>
2019-05-17 19:47:42 -0300

    Remove text

--- components/listings/buy/BuyHeader/index.js
+++ components/listings/buy/BuyHeader/index.js
@@ -9,12 +9,8 @@ class BuyHeader extends Component {
       <Wrapper>
         <Container>
           <Title as="h2" fontWeight="bold" fontSize={[2, null, null, 4]}>
-            Busque seu imóvel <TextHighlight>sem sair de casa.</TextHighlight><br /> Sem corretores <TextHighlight strokeWidth="3.8">empurrando imóveis</TextHighlight>
+            Deixe <TextHighlight>nossos especialistas</TextHighlight> encontrarem o imóvel ideal para você.
           </Title>
-          <Text fontSize={[1, null, null, 2]}>
-            Visite imóveis através do <strong>Tour Virtual</strong> e fale com
-            um dos nossos <strong>Especialistas</strong> de onde estiver.
-          </Text>
         </Container>
       </Wrapper>
     )
