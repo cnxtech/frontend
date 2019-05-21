51796d3152d4037e739c840c381e7c6fdf5474ec
Lucas Tulio <lucasctulio@gmail.com>
2019-05-21 15:59:25 -0300

    Refactor and test BuyBar component

---
+++ __tests__/components/shared/BuyBar/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import BuyBar from 'components/shared/BuyBar'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import theme from 'config/theme'
+import { ThemeProvider } from 'styled-components'
+import { MockedProvider } from 'react-apollo/test-utils'
+
+describe('<BuyBar/>', () => {
+  it('should render component', () => {
+    const tree = renderer.create(
+      <ThemeProvider theme={theme}>
+        <MockedProvider>
+          <BuyBar
+            user={{}}
+          />
+        </MockedProvider>
+      </ThemeProvider>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
