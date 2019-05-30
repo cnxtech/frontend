429802f6e5754afae1f9c3675afb2c01ff8d7fed
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-05-30 17:11:12 -0300

    Style ActionBar favorite button

--- components/shared/ActionsBar/index.js
+++ components/shared/ActionsBar/index.js
@@ -1,5 +1,7 @@
-import {Component, Fragment} from 'react'
+import {Component} from 'react'
 import Link from 'next/link'
+import {graphql} from 'react-apollo'
+import {compose} from 'recompose'
 import PropTypes from 'prop-types'
 import Text from '@emcasa/ui-dom/components/Text'
 import Row from '@emcasa/ui-dom/components/Row'
@@ -7,33 +9,61 @@ import Icon from '@emcasa/ui-dom/components/Icon'
 import Filter from 'components/shared/Filter'
 import {Wrapper, Container, FavCount} from './styles'
 import {DEFAULT_CITY} from 'utils/location-utils'
+import {GET_USER_LISTINGS_ACTIONS} from 'graphql/user/queries'
 import ActionButton from './Button'
 
 class ActionsBar extends Component {
   static Button = ActionButton
 
+  static defaultProps = {
+    filters: {},
+    favorites: [],
+    currentCity: DEFAULT_CITY
+  }
+
+  static propTypes = {
+    filters: PropTypes.object,
+    currentCity: PropTypes.object,
+    onSubmit: PropTypes.func.isRequired,
+    favorites: PropTypes.array,
+    isFavoritesPage: PropTypes.bool
+  }
+
   getFavoritesCountDisplay() {
-    const {user, favorites} = this.props
-    if (user && user.authenticated) {
+    const {user, favorites, isFavoritesPage} = this.props
+    if (user && user.authenticated && !isFavoritesPage) {
       return (
-        <Link href={'/meu-perfil/favoritos'}>
-          {this.favoritesCount(user, favorites)}
+        <Link passHref href={'/meu-perfil/favoritos'}>
+          <a>
+            {this.favoritesCount(user, favorites)}
+          </a>
         </Link>
       )
     }
     return this.favoritesCount(user, favorites)
   }
 
-  favoritesCount = (user, favorites) => (
-    <FavCount authenticated={user && user.authenticated}>
-      <Icon name="heart" size={16} />
-      <Text fontSize={1}>
-        Favoritos<Text fontSize={1} inline color="pink">
-          {user && user.authenticated && favorites ? favorites.length : 0}
+  favoritesCount = (user, favorites) => {
+    const {isFavoritesPage} = this.props
+    return (
+      <FavCount authenticated={user && user.authenticated}>
+        <Icon
+          mr={2}
+          name="heart"
+          type={isFavoritesPage ? 'default' : 'light'}
+          size={20}
+          color={isFavoritesPage ? 'pink' : 'dark'}
+        />
+        <Text fontSize={1}>
+          Favoritos
+          {String.fromCharCode(0x00a0)}
+          <Text fontSize={1} inline color="pink">
+            {favorites.length}
+          </Text>
         </Text>
-      </Text>
-    </FavCount>
-  )
+      </FavCount>
+    )
+  }
 
   render() {
     const {button, onSubmit, filters, currentCity} = this.props
@@ -57,14 +87,11 @@ class ActionsBar extends Component {
   }
 }
 
-ActionsBar.defaultProps = {
-  filters: {},
-  currentCity: DEFAULT_CITY
-}
-ActionsBar.propTypes = {
-  filters: PropTypes.object,
-  currentCity: PropTypes.object,
-  onSubmit: PropTypes.func.isRequired,
-  favorites: PropTypes.array
-}
-export default ActionsBar
+export default compose(
+  graphql(GET_USER_LISTINGS_ACTIONS, {
+    skip: ({user}) => !user.authenticated,
+    props: ({data: {userProfile}}) => ({
+      favorites: (userProfile && userProfile.favorites) || []
+    })
+  })
+)(ActionsBar)
