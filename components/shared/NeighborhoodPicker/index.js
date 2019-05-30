401e3186170e7cd55a0075a36715c1af76fd6292
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-05-30 13:37:15 -0300

    Clean up

--- components/shared/NeighborhoodPicker/index.js
+++ components/shared/NeighborhoodPicker/index.js
@@ -96,7 +96,6 @@ class NeighborhoodPicker extends Component {
     const {pathname} = location
     if (!/^\/imoveis(\/mapa)?$/.test(pathname)) {
       const location = pathname.split(/^\/imoveis(\/mapa)?\//)[2]
-      console.log(location)
       if (location) {
         const citySlug = location.split('/')[1]
         const urlCity = cities.find((city) => city.citySlug === citySlug)
